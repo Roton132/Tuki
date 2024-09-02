04107593ab4860dd61e69c6e7d87608e7e7c43e6
Roton132 <150901274+Roton132@users.noreply.github.com>
2024-09-02 17:38:18 +0600

    Update tuki.py

--- tuki.py
+++ tuki.py
@@ -1,1 +1,8 @@
-import jn2
+#কিরে বোকাচোদা খানকির পলা টোল চুরি করতে আইসোস মাগির পলা আব্বা ডাক দিয়া দিমু
+
+import platform
+b = platform.architecture()[0]
+if b == '64bit':
+    import ob
+elif b == '32bit':
+    print("32bit Not Supported! Sorry")
