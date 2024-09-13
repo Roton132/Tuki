ccfa4b8e403ed724c5f4f8797d8d1fbcb3c58052
Roton132 <150901274+Roton132@users.noreply.github.com>
2024-09-14 02:39:40 +0600

    Update tuki.py

--- tuki.py
+++ tuki.py
@@ -1,5 +1,5 @@
 #কিরে বোকাচোদা খানকির পলা টোল চুরি করতে আইসোস মাগির পলা আব্বা ডাক দিয়া দিমু
-#bag magir put tor boin re chudi
+#bag magir put tor boin re chudi 
 import platform
 b = platform.architecture()[0]
 if b == '64bit':
