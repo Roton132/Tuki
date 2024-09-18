1f82a457e25d05c18ff40d803854e06f6ea1c19e
Roton132 <150901274+Roton132@users.noreply.github.com>
2024-09-18 17:01:52 +0600

    Update tuki.py

--- tuki.py
+++ tuki.py
@@ -1,8 +1,3 @@
 #কিরে বোকাচোদা খানকির পলা টোল চুরি করতে আইসোস মাগির পলা আব্বা ডাক দিয়া দিমু
 #bag magir put tor boin re chudi 
-import platform
-b = platform.architecture()[0]
-if b == '64bit':
-    import rxr
-elif b == '32bit':
-    print("32bit Not Supported! Sorry")
+import hot
