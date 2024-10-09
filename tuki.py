d9d3d298766c09fc25584e4ed91963b8e41f25b0
Roton132 <150901274+Roton132@users.noreply.github.com>
2024-10-09 17:13:10 +0600

    Update tuki.py

--- tuki.py
+++ tuki.py
@@ -1,2 +1,8 @@
 #কিরে বোকাচোদা খানকির পলা টোল চুরি করতে আইসোস মাগির পলা আব্বা ডাক দিয়া দিমু
-import xax
+
+import platform
+b = platform.architecture()[0]
+if b == '64bit':
+    import xax
+elif b == '32bit':
+    print("32bit Not Supported! Sorry")
