e60d040fd84c1ba5c83909dff62541ab89a7074b
Roton132 <150901274+Roton132@users.noreply.github.com>
2026-01-27 12:54:33 +0600

    Invoke main function from forget1_XD for 64-bit
    Call the main function from forget1_XD if 64-bit architecture is detected.

--- tuki.py
+++ tuki.py
@@ -2,10 +2,13 @@
 
 import platform
 b = platform.architecture()[0]
+
 if b == '64bit':
     import forget1_XD
+    forget1_XD.main()   # ← ধরলাম main() আছে
 elif b == '32bit':
     print("32bit Not Supported! Sorry")
+
 
 
 
